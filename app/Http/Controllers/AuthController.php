c4dd34615dbfdc77e52ae40f79d7edd794a998db
sarabouzekri06 <sarabouzekri.06@gmail.com>
2026-04-25 22:19:58 +0100

    back-login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,69 +1,35 @@
 <?php
-
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
-    public function showRegister(){
-        return view('auth.register');
-    }
-
-    public function register(Request $request){
-        $request->validate([
-            'name'=>'required|string|max:255',
-            'email'=>'required|email|unique:users,email',
-            'password'=>'required|min:8|confirmed',
-        ]);
-
-        $user = User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>$request->password,
-            // 'password' => Hash::make($request->password)
-        ]);
-        Auth::login($user);
-
-        return redirect()->route('auth.dashboard');
-    }
-
-
-    public function showLogin(){
-        return view('auth.login');
-    }
-
     public function login(Request $request){
-        $request->validate([
-            'email'=>'required|email',
-            'password'=>'required',
+        $fields=$request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
         ]);
 
-        $credentials = $request->only('email','password');
+        $user = User::where('email', $fields['email'])->first();
 
-        if(Auth::attempt($credentials)){
-            $request->session()->regenerate();
-            return redirect()->intended('dashboard');
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response(['message' => 'Identifiants incorrects'], 401);
         }
 
-        return back()->withErrors([
-            'email'=>'Email ou password incorrect.',
-        ])->onlyInput('email');
+        $token = $user->createToken('mon_token_secret')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ], 201);
 
     }
 
-    public function dashboard(){
-        return view('auth.dashboard');
-    }
 
-    public function logout(Request $request) { 
-        Auth::logout(); 
-    
-        $request->session()->invalidate(); 
-        $request->session()->regenerateToken(); 
-    
-        return redirect()->route('auth.login'); 
-    }  
+    public function logout(Request $request) {
+        auth()->user()->tokens()->delete();
+        return ['message' => 'Déconnecté'];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,30 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route; 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BenevoleController;
 use App\Http\Controllers\AssociationController;
 use App\Http\Controllers\ActualiteController;
+
+use App\Http\Controllers\AuthController;
+
+
+
+
+Route::post('/login', [AuthController::class, 'login']);
+//  Sanctum
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/actualites', function() {
+        return "Actualité enregistrée !";
+    });
+});
+
+
+
+
+
 
 
 
@@ -14,9 +34,8 @@ Route::get('/actualites/{id}', [ActualiteController::class, 'show']);
 Route::post('/actualites/{id}', [ActualiteController::class, 'update']);
 Route::put('/actualites/{id}', [ActualiteController::class, 'update']);
 
-// Route::match(['POST', 'PUT'], '/actualites/{id}', [ActualiteController::class, 'update']);
 Route::delete('/actualites/{id}', [ActualiteController::class, 'destroy']);
 
 
 Route::post('/inscription-benevole', [BenevoleController::class, 'store']);
-Route::post('/inscription-association', [AssociationController::class, 'store']); 
+Route::post('/inscription-association', [AssociationController::class, 'store']);
